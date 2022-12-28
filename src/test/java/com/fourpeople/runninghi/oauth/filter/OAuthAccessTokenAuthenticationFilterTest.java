a39ef83c99dab76763d5c1a4ee37bc7a4039d432
seung-woo-ryu <tmddn645@naver.com>
2022-12-28 23:21:12 +0900

    feat: 테스트 코드 작성

--- src/test/java/com/fourpeople/runninghi/oauth/filter/OAuthAccessTokenAuthenticationFilterTest.java
+++ src/test/java/com/fourpeople/runninghi/oauth/filter/OAuthAccessTokenAuthenticationFilterTest.java
@@ -1,12 +1,15 @@
 package com.fourpeople.runninghi.oauth.filter;
 
+import com.fourpeople.runninghi.common.utils.ApiUtils;
 import com.fourpeople.runninghi.oauth.entity.OAuthAccessToken;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,10 +22,14 @@ class OAuthAccessTokenAuthenticationFilterTest {
     void filter_test() {
         OAuthAccessToken oAuthAccessToken = new OAuthAccessToken("12313123", "kakao");
 
-        webTestClient.get()
-                .uri("oauth/test")
-                //.body(BodyInserters.fromValue(oAuthAccessToken))
+        webTestClient.post()
+                .uri("/oauth/signin/kakao")
+                .body(BodyInserters.fromValue(oAuthAccessToken))
                 .exchange()
+
+                .expectStatus()
+                .is2xxSuccessful()
+
                 .expectBody()
                 .consumeWith(System.out::println);
     }
