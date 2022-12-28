7408a6851be2c8e535a20b0ffb4ce21b12c04c39
seung-woo-ryu <tmddn645@naver.com>
2022-12-28 23:16:25 +0900

    feat: kakao 로그인 controller, service

--- src/main/java/com/fourpeople/runninghi/oauth/controller/OAuthController.java
+++ src/main/java/com/fourpeople/runninghi/oauth/controller/OAuthController.java
@@ -1,7 +1,18 @@
 package com.fourpeople.runninghi.oauth.controller;
 
+import com.fourpeople.runninghi.common.utils.ApiUtils;
+import com.fourpeople.runninghi.common.utils.ApiUtils.ApiResult;
+import com.fourpeople.runninghi.oauth.service.OAuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/oauth")
+@RequiredArgsConstructor
 public class OAuthController {
-    @PostMapping("/signin")
-    public String signin() {
-        return "signin success";
+    private final OAuthService oAuthService;
+
+    @PostMapping("/signin/kakao")
+    public ResponseEntity<ApiResult<?>> signin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = (String) authentication.getPrincipal();
+
+        return new ResponseEntity<>(oAuthService.signin(email), HttpStatus.OK);
     }
 
     @GetMapping("/test")
--- src/main/java/com/fourpeople/runninghi/oauth/service/OAuthService.java
+++ src/main/java/com/fourpeople/runninghi/oauth/service/OAuthService.java
@@ -1,2 +1,29 @@
-package com.fourpeople.runninghi.oauth.service;public class OAuthService {
+package com.fourpeople.runninghi.oauth.service;
+
+import com.fourpeople.runninghi.common.utils.ApiUtils;
+import com.fourpeople.runninghi.common.utils.ApiUtils.ApiResult;
+import com.fourpeople.runninghi.config.auth.JWTManager;
+import com.fourpeople.runninghi.config.auth.JWTProperties;
+import com.fourpeople.runninghi.config.auth.PrincipalDetails;
+import com.fourpeople.runninghi.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.fourpeople.runninghi.common.utils.ApiUtils.success;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OAuthService {
+    private final UserRepository userRepository;
+    public ApiResult<?> signin(String email) {
+        return success(userRepository.findByEmail(email)
+                .map((user) -> JWTManager.makeUserBothToken(new PrincipalDetails(user)))
+                .orElse(JWTManager.makeGuestBothToken()));
+    }
 }
