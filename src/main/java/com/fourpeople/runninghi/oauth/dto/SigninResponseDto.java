3a6d7026e4ac7d79e4b5964c406c6ea4e3b95db5
seung-woo-ryu <tmddn645@naver.com>
2022-12-28 23:18:30 +0900

    feat: OAuth login result dto

--- src/main/java/com/fourpeople/runninghi/oauth/dto/SigninResponseDto.java
+++ src/main/java/com/fourpeople/runninghi/oauth/dto/SigninResponseDto.java
@@ -1,2 +1,25 @@
-package com.fourpeople.runninghi.oauth.dto;public class signinResponseDto {
+package com.fourpeople.runninghi.oauth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class SigninResponseDto {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    private SigninResponseDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public static SigninResponseDto of(String accessToken, String refreshToken){
+        return new SigninResponseDto(accessToken, refreshToken);
+    }
 }
