7d8f265a3ba0bec07975ec771003e1a677efb009
seung-woo-ryu <tmddn645@naver.com>
2022-12-28 23:19:03 +0900

    feat: add authentication exceptionHandler

--- src/main/java/com/fourpeople/runninghi/oauth/exception/OAuthAuthenticationEntryPoint.java
+++ src/main/java/com/fourpeople/runninghi/oauth/exception/OAuthAuthenticationEntryPoint.java
@@ -1,18 +1,24 @@
 package com.fourpeople.runninghi.oauth.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fourpeople.runninghi.common.utils.ApiUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static com.fourpeople.runninghi.common.utils.ApiUtils.error;
+
 @Slf4j
 @Component
 public class OAuthAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void commence(
             HttpServletRequest request,
@@ -22,10 +28,12 @@ public class OAuthAuthenticationEntryPoint implements AuthenticationEntryPoint {
         authException.printStackTrace();
         log.info("Responding with unauthorized error. Message := {}", authException.getMessage());
 
-        response.sendError(
-                HttpServletResponse.SC_UNAUTHORIZED,
-                authException.getLocalizedMessage()
-        );
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
 
+        response.getWriter()
+                .write(objectMapper.writeValueAsString(
+                        error(authException.getMessage(), HttpStatus.UNAUTHORIZED)));
     }
 }
