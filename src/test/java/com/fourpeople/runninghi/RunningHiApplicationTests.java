02682681bba361e88fcdac7c512035f30bd636cb
seung-woo-ryu <tmddn645@naver.com>
2023-01-07 19:42:44 +0900

    feat: add code-coverage.yml

--- src/test/java/com/fourpeople/runninghi/RunningHiApplicationTests.java
+++ src/test/java/com/fourpeople/runninghi/RunningHiApplicationTests.java
@@ -1,7 +1,9 @@
 package com.fourpeople.runninghi;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RunningHiApplicationTests {
