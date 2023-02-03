c701cf31fde5e0f738253900b49988baf7faf2c8
디지털채널시스템팀/유승우 <1193687@device.autoever.com>
2023-02-03 15:40:48 +0900

    tmp

--- src/main/java/com/fourpeople/runninghi/oauth/filter/CustomBearerTokenAuthenticationFilter.java
+++ src/main/java/com/fourpeople/runninghi/oauth/filter/CustomBearerTokenAuthenticationFilter.java
@@ -1,2 +1,65 @@
-package com.fourpeople.runninghi.oauth.filter;public class CustomBearerTokenAuthenticationFilter {
+package com.fourpeople.runninghi.oauth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.log.LogMessage;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class CustomBearerTokenAuthenticationFilter extends OncePerRequestFilter {
+    private BearerTokenResolver bearerTokenResolver = new DefaultBearerTokenResolver();
+    private SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
+            .getContextHolderStrategy();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token;
+        try {
+            token = this.bearerTokenResolver.resolve(request);
+        } catch (OAuth2AuthenticationException invalid) {
+            this.logger.trace("Sending to authentication entry point since failed to resolve bearer token", invalid);
+            this.authenticationEntryPoint.commence(request, response, invalid);
+            return;
+        }
+
+        if (token == null) {
+            this.logger.trace("Did not process request since did not find bearer token");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        BearerTokenAuthenticationToken authenticationRequest = new BearerTokenAuthenticationToken(token);
+        authenticationRequest.setDetails(this.authenticationDetailsSource.buildDetails(request));
+
+        try {
+            AuthenticationManager authenticationManager = this.authenticationManagerResolver.resolve(request);
+            Authentication authenticationResult = authenticationManager.authenticate(authenticationRequest);
+            SecurityContext context = this.securityContextHolderStrategy.createEmptyContext();
+            context.setAuthentication(authenticationResult);
+            this.securityContextHolderStrategy.setContext(context);
+            this.securityContextRepository.saveContext(context, request, response);
+            if (this.logger.isDebugEnabled()) {
+                this.logger.debug(LogMessage.format("Set SecurityContextHolder to %s", authenticationResult));
+            }
+            filterChain.doFilter(request, response);
+        }
+        catch (AuthenticationException failed) {
+            this.securityContextHolderStrategy.clearContext();
+            this.logger.trace("Failed to process authentication request", failed);
+            this.authenticationFailureHandler.onAuthenticationFailure(request, response, failed);
+        }
+    }
 }
--- src/test/java/com/fourpeople/runninghi/RunningHiApplicationTests.java
+++ src/test/java/com/fourpeople/runninghi/RunningHiApplicationTests.java
@@ -1,15 +1,30 @@
 package com.fourpeople.runninghi;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.util.Assert;
+
+import java.util.List;
 
 @SpringBootTest
 class RunningHiApplicationTests {
 
+	@Autowired
+	private FilterChainProxy filterChainProxy;
 	@Test
 	void contextLoads() {
+		filterChainProxy
+				.getFilterChains()
+				.stream()
+				.flatMap(chain->chain.getFilters()
+						.stream())
+				.forEach(f-> System.out.println(f.getClass()));
+
 	}
 
 }
